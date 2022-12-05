c623bd1855a97b5d174dba1bf6afc75e00570b14
SwaritMisro12 <95137718+SwaritMisro12@users.noreply.github.com>
2022-12-05 15:23:53 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,22 @@
+import flusk from libary*
+import os
+import random
+import Littlerr
+
+
+    #This  REVIEW: Indexes Bar For Ping
+if(index = afk(-10)):
+     def index.print("Afk Hoster")
+ else:
+    if(index("Local = Condition")=+Afk(--10)))
+    file_read=(section.[config.yml])
+           return;
+
+
+            #The Ping Of Refer = Ms
+if (os.py = 10):
+elif:
+    (os.ss = 10):
+def = debug.if-main["(((((10)))))"]
+           print("Pls Add System Time Vaule To Ping The Website Of Auto_Afk")
+           return;
